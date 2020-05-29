ba18821a59766aca05c0dff90f9c69574bd74134
MatiBiela <mateuszbiela9@gmail.com>
2020-05-29 08:46:09 +0200

    nie umiem

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+	void wypiszLiczbeLosowa( int 11 )
+	{
+		int liczba =rand()%11
+	}
+	void wypiszLiczbeJSON( int 12)
+	{
+	
+	}
+
+int main(int argc, char** argv) {
+	{
+		srand ( time( NULL) );
+	}
+	
+
+
+	cout<<liczba;
+
+	ofstream wyjscie;
+	wyjscie.open("out.json");
+		{
+			cout<<LiczbaJSON;
+		}
+	
+	
+		
+	return 0;
+}
